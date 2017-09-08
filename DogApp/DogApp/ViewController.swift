f81fe93631895966dee98e4accef70503f02993a
Aeec <Aeec@users.noreply.github.com>
2017-09-08 12:15:02 +0200

    first commit

---
+++ DogApp/DogApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogApp
+//
+//  Created by Isay González Lázaro on 8/9/17.
+//  Copyright © 2017 Isay González Lázaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
